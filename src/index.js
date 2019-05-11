5a24f54f4b51f2379fa5472599f4e9b8ba3ae2f4
Agnieszka Iwanaszko <a.iwanaszko@gmail.com>
2019-05-11 18:17:38 +0200

    Test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const test = "1"
-
 import React from 'react';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+
+const test = "1"
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
